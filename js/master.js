30e81f916bb10a0e9b815726f500a7414af9a97a
ajeeshms <ajeesh@paskan.com>
2014-05-09 13:04:50 +0530

    Home Page Design

---
+++ js/master.js
@@ -0,0 +1,27 @@
+﻿$(function () {
+    var bdy = $(document.body);
+    var home_page = $('#home-page');
+
+    //Homepage Background Slideshow
+    if (home_page.length > 0) {
+        var background_urls = ['/img/bg/bg1.jpg', '/img/bg/bg2.jpg', '/img/bg/bg3.jpg', '/img/bg/bg4.jpg'];
+        $.each(background_urls, function (i, url) {
+            var img = $('<img src="' + url + '"/>');
+            img.ready(function () {
+                var panel = $('<div class="bg-panel"></div>');
+                if (i == 0) {
+                    panel.addClass('current');
+                }
+                panel.css('background-image', 'url("' + url + '")');
+                home_page.append(panel);
+            });
+        });
+        setInterval(function () {
+            var current = home_page.find('.bg-panel.current');
+            var next = current.next('.bg-panel');
+            next = next.length == 0 ? $('.bg-panel:first') : next;
+            current.removeClass('current');
+            next.addClass('current');
+        }, 5000);
+    }
+});
